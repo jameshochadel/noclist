efba1a0e81acf826bc81bceb114f6a0ee5f5d2ea
James Hochadel <james.hochadel@hey.com>
2022-01-21 15:08:42 -0500

    Make authenticated request to /users

--- internal/noclist/client.go
+++ internal/noclist/client.go
@@ -1,8 +1,12 @@
 package noclist
 
 import (
+	"crypto/sha256"
 	"errors"
+	"fmt"
+	"io"
 	"net/http"
+	"strings"
 )
 
 const (
@@ -13,6 +17,7 @@ const (
 var (
 	ErrAuthentication = errors.New("client: authentication to server failed")
 	ErrTimeout        = errors.New("client: hit maximum retries")
+	ErrFailed         = errors.New("client: request failed")
 )
 
 // Client wraps calls to the BADSEC API.
@@ -30,6 +35,30 @@ func (c *Client) authenticate() error {
 	}
 	c.token = resp.Header.Get(headerBadsecAuthenticationToken)
 	return nil
+}
+
+// checksum should only be called on Clients created via noclist.New().
+func (c *Client) checksum(path string) string {
+	s := sha256.New()
+	s.Write([]byte(c.token + path))
+	return fmt.Sprintf("%x", s.Sum(nil))
+}
+
+func (c *Client) ListUsers() ([]string, error) {
+	path := "/users"
+	req, err := http.NewRequest("GET", c.baseURL+path, nil)
+	req.Header.Set(headerRequestChecksum, c.checksum(path))
+
+	resp, err := c.client.Do(req)
+	if err != nil || resp.StatusCode != 200 {
+		return nil, ErrFailed
+	}
+
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, ErrFailed
+	}
+	return strings.Split(string(b), "\n"), nil
 }
 
 // New returns a Client that is ready to make authenticated requests to the server,
--- main.go
+++ main.go
@@ -12,6 +12,8 @@ func main() {
 			ServerURL: "http://localhost:8888",
 		},
 	)
+	u, err := client.ListUsers()
 	fmt.Printf("err: %v\n", err)
 	fmt.Printf("client: %v\n", client)
+	fmt.Printf("u: %v\n", u)
 }
